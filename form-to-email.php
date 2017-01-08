96df59bbe329d1fe7ee4e7b315b545dd445e7212
connordear <connordear@gmail.com>
2017-01-08 12:02:14 -0500

    updated form

--- form-to-email.php
+++ form-to-email.php
@@ -9,7 +9,7 @@ $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
 //Validate first
-if(empty($name)||empty($visitor_email)) 
+if(empty($name)||empty($visitor_email))
 {
     echo "Name and email are mandatory!";
     exit;
@@ -21,12 +21,12 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_from = 'connordear@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
-    
-$to = "tom@amazing-designs.com";//<== update the email address
+
+$to = "connordear@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
@@ -57,5 +57,5 @@ function IsInjected($str)
     return false;
   }
 }
-   
-?> 
+
+?>
