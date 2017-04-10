047ff49ce5520305432995841c57da5536ce3cd3
Connor Dear <connordear@Connors-MacBook-Pro.local>
2017-04-09 22:11:38 -0400

    New custom webpage

---
+++ script.js
@@ -0,0 +1,22 @@
+// var winWidth = 0;
+//
+// $(document).ready(function() {
+//   setFourContainer();
+//   setContentWidth();
+//   main();
+//   winWidth = parseInt($(window).width());
+//   function setFourContainer() {
+//       $('.four-container').css({"width":winWidth/4});
+//   }
+//   function setContentWidth() {
+//     if(winWidth >= 1746) {
+//       $('#content').css({"width":1746});
+//     } else {
+//       $('#content').css({"width":winWidth});
+//     };
+//   }
+// });
+
+$(document).ready(function() {
+  
+})
